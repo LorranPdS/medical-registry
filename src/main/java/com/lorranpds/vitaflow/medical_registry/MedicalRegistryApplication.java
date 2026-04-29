fe800d81f52a01d7f5caa6241733a7ea12c9bc97
LorranPdS <lorransantospereira@yahoo.com.br>
2026-04-29 10:58:14 -0300

    feat: initial commit

---
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/MedicalRegistryApplication.java
@@ -0,0 +1,10 @@
+package com.lorranpds.vitaflow.medical_registry;
+
+public class MedicalRegistryApplication {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/lorranpds/vitaflow/medical_registry/MedicalRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorranpds.vitaflow.medical_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
