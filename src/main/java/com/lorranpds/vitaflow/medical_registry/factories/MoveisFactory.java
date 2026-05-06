592bae79166515c47b3aa1b1acdf3750b3702cbf
LorranPdS <lorransantospereira@yahoo.com.br>
2026-05-06 12:07:19 -0300

    refactor: deleting unused files

--- src/main/java/com/lorranpds/vitaflow/medical_registry/factories/MoveisFactory.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/factories/MoveisFactory.java
@@ -5,6 +5,11 @@ import com.lorranpds.vitaflow.medical_registry.enums.EstiloMovelEnum;
 import com.lorranpds.vitaflow.medical_registry.services.Cadeira;
 import com.lorranpds.vitaflow.medical_registry.services.Sofa;
 
+/* SOBRE O NOME "MoveisFactory" para o padrão "Abstract Factory":
+    - manter o nome 'MoveisFactory'.
+    - O que define que ele é um Abstract Factory não é o nome da interface, mas sim o fato de ela
+        possuir múltiplos métodos de criação para produtos diferentes (famílias) que devem combinar entre si.
+ */
 public interface MoveisFactory {
     EstiloMovelEnum getEstilo();
     Cadeira criarCadeira();
