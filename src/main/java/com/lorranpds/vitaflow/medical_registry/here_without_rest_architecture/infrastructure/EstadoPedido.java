2e4e32bf5282a3f4e7e98cb7f301b8baed791379
LorranPdS <lorransantospereira@yahoo.com.br>
2026-06-12 15:13:12 -0300

    style: improving error message in EstadoPedido

--- src/main/java/com/lorranpds/vitaflow/medical_registry/here_without_rest_architecture/infrastructure/EstadoPedido.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/here_without_rest_architecture/infrastructure/EstadoPedido.java
@@ -4,17 +4,18 @@ import com.lorranpds.vitaflow.medical_registry.here_without_rest_architecture.se
 
 // 1. Interface State
 public interface EstadoPedido {
+
     // Comportamento padrão: se o estado não reescrever o metodo, a ação é bloqueada.
     default void pagar(Pedido pedido) {
-        throw new IllegalStateException("Ação 'Pagar' não permitida no estado atual para o pedido [id="+pedido.getId()+"].");
+        throw new IllegalStateException("Ação 'Pagar' não permitida no estado atual "+pedido.getEstado().getClass().getSimpleName()+" para o pedido [id="+pedido.getId()+"].");
     }
     default void cancelar(Pedido pedido) {
-        throw new IllegalStateException("Ação 'Cancelar' não permitida no estado atual para o pedido [id="+pedido.getId()+"].");
+        throw new IllegalStateException("Ação 'Cancelar' não permitida no estado atual "+pedido.getEstado().getClass().getSimpleName()+" para o pedido [id="+pedido.getId()+"].");
     }
     default void enviar(Pedido pedido) {
-        throw new IllegalStateException("Ação 'Enviar' não permitida no estado atual para o pedido [id="+pedido.getId()+"].");
+        throw new IllegalStateException("Ação 'Enviar' não permitida no estado atual "+pedido.getEstado().getClass().getSimpleName()+" para o pedido [id="+pedido.getId()+"].");
     }
     default void entregar(Pedido pedido) {
-        throw new IllegalStateException("Ação 'Entregar' não permitida no estado atual para o pedido [id="+pedido.getId()+"].");
+        throw new IllegalStateException("Ação 'Entregar' não permitida no estado atual "+pedido.getEstado().getClass().getSimpleName()+" para o pedido [id="+pedido.getId()+"].");
     }
 }
