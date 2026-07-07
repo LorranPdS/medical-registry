ed8f67f24eb13acc876f01cf297b0c51d6efb3f7
LorranPdS <lorransantospereira@yahoo.com.br>
2026-07-07 11:16:00 -0300

    fix: remove data annotation and write only getters and setters

--- src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/model/Artigo.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/model/Artigo.java
@@ -1,6 +1,7 @@
 package com.lorranpds.vitaflow.medical_registry.using_spring_rest.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
@@ -13,23 +14,31 @@ import java.util.UUID;
 
         A nossa Entidade passa a carregar o seu identificador público universal.
  */
-@Data
+@Getter
 public class Artigo {
 
     // Se optar pela estratégia de ID Duplo:
     private Long idInterno;
-    private UUID codigoPublico = UUID.randomUUID(); // Nasce blindado com um hash aleatório
+
+    // O UUID nasce blindado aqui no nascimento do objeto com um hash aleatório
+    private UUID codigoPublico = UUID.randomUUID();
+
+    // Apenas os textos ganham @Setter, pois são os únicos que o jornalista pode mudar
+    @Setter
     private String titulo;
+    @Setter
     private String texto;
 
+    // 1. Construtor Vazio: Obrigatório para o JPA/Hibernate trabalhar internamente
     public Artigo() {}
 
+    // 2. Construtor de Negócio: O único que o seu Service vai usar para criar novas matérias
     public Artigo(String titulo, String texto) {
         this.titulo = titulo;
         this.texto = texto;
     }
 
-    // Regra do Memento: Gera a cápsula do tempo
+    // Regra do Memento: Gera a cápsula do tempo. Permanecem isoladas e protegidas aqui para baixo...
     public ArtigoMemento criarSnapshot() {
         return new ArtigoMemento(this.titulo, this.texto);
     }
