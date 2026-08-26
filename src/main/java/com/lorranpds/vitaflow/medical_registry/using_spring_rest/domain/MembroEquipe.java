6e5b0eb5738e451188bdc4f0b232128adcbdc9e7
LorranPdS <lorransantospereira@yahoo.com.br>
2026-08-26 10:01:20 -0300

    style: correcting comment

--- src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/domain/MembroEquipe.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/domain/MembroEquipe.java
@@ -1,6 +1,6 @@
 package com.lorranpds.vitaflow.medical_registry.using_spring_rest.domain;
 
-// 1. Composite - interface abstrata em comum
+// 1. Component - interface comum
 public interface MembroEquipe {
 
     // Não considerei os métodos como getters para compilar com a record sem acréscimos
