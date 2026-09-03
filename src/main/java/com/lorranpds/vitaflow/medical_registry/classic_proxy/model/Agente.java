ecd727166475b7f82b0cf9a84f85b37b70198ef3
LorranPdS <lorransantospereira@yahoo.com.br>
2026-09-03 11:27:15 -0300

    refactor: including validations for the agente atributtes

--- src/main/java/com/lorranpds/vitaflow/medical_registry/classic_proxy/model/Agente.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/classic_proxy/model/Agente.java
@@ -1,4 +1,11 @@
 package com.lorranpds.vitaflow.medical_registry.classic_proxy.model;
 
-// Nosso Record representando o usuário logado
-public record Agente(String nome, Cargo cargo){}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+/*
+    1) nosso record está representando o usuário logado.
+    2) os parâmetros do objeto nunca nascerão sem dados já que estou colocando @NotBlank e @NotNull neles,
+        logo, qualquer validação posterior se é nula seria "redundância defensiva desnecessária"
+ */
+public record Agente(@NotBlank String nome, @NotNull Cargo cargo){}
