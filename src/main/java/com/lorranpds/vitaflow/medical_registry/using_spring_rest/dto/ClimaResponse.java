df738c171dd0215ce08780494e8b3712032ba193
LorranPdS <lorransantospereira@yahoo.com.br>
2026-09-07 13:41:49 -0300

    refactor: rename from ClimaDto to ClimaResponse

--- src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/dto/ClimaResponse.java
+++ src/main/java/com/lorranpds/vitaflow/medical_registry/using_spring_rest/dto/ClimaResponse.java
@@ -1,5 +1,5 @@
 package com.lorranpds.vitaflow.medical_registry.using_spring_rest.dto;
 
 // Nosso record de retorno
-public record ClimaDto(String cidade, double temperatura, String condicao) {
+public record ClimaResponse(String cidade, double temperatura, String condicao) {
 }
